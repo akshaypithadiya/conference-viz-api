1fc61f68dcb445fddd9301724ed42c208025f3e0
Akshay Pithadiya <akshay.pithadiya@gmail.com>
2020-12-08 22:35:37 +0530

    comference details updated

--- models/conferences.py
+++ models/conferences.py
@@ -3,16 +3,25 @@ import json
 conferences = []
 
 def load_conference():
-	iclr_data = open('./data/ICLR/conference.json','r')
-	cvpr_data = open('./data/CVPR/conference.json', 'r')
 	acl_data = open('./data/ACL/conference.json','r')
+	cvpr_data = open('./data/CVPR/conference.json', 'r')
+	emnlp_data = open('./data/EMNLP/conference.json', 'r')
+	iclr_data = open('./data/ICLR/conference.json','r')
+	icml_data = open('./data/ICML/conference.json', 'r')
+	nips_data = open('./data/NIPS/conference.json', 'r')
 
-	iclr = json.load(iclr_data)
-	cvpr = json.load(cvpr_data)
 	acl = json.load(acl_data)
+	cvpr = json.load(cvpr_data)
+	emnlp = json.load(emnlp_data)
+	iclr = json.load(iclr_data)
+	icml = json.load(icml_data)
+	nips = json.load(nips_data)
 
-	conferences.append(iclr)
-	conferences.append(cvpr)
 	conferences.append(acl)
+	conferences.append(cvpr)
+	conferences.append(emnlp)
+	conferences.append(iclr)
+	conferences.append(icml)
+	conferences.append(nips)
 
 load_conference()
