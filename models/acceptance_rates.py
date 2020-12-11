3b91885f4ccdca56a3585f1d6e760730b68838a6
Akshay Pithadiya <akshay.pithadiya@gmail.com>
2020-12-12 01:16:42 +0530

    added acceptance-rate for all conferences

--- models/acceptance_rates.py
+++ models/acceptance_rates.py
@@ -3,16 +3,25 @@ import json
 acceptance_rates = []
 
 def load_acceptance_rate():
-	iclr_data = open('./data/ICLR/acceptance_rate.json','r')
-	cvpr_data = open('./data/CVPR/acceptance_rate.json', 'r')
 	acl_data = open('./data/ACL/acceptance_rate.json','r')
+	cvpr_data = open('./data/CVPR/acceptance_rate.json', 'r')
+	emnlp_data = open('./data/EMNLP/acceptance_rate.json', 'r')
+	iclr_data = open('./data/ICLR/acceptance_rate.json','r')
+	icml_data = open('./data/ICML/acceptance_rate.json', 'r')
+	nips_data = open('./data/NIPS/acceptance_rate.json', 'r')
 
-	iclr = json.load(iclr_data)
-	cvpr = json.load(cvpr_data)
 	acl = json.load(acl_data)
+	cvpr = json.load(cvpr_data)
+	emnlp = json.load(emnlp_data)
+	iclr = json.load(iclr_data)
+	icml = json.load(icml_data)
+	nips = json.load(nips_data)
 
-	acceptance_rates.append(iclr)
-	acceptance_rates.append(cvpr)
 	acceptance_rates.append(acl)
+	acceptance_rates.append(cvpr)
+	acceptance_rates.append(emnlp)
+	acceptance_rates.append(iclr)
+	acceptance_rates.append(icml)
+	acceptance_rates.append(nips)
 
 load_acceptance_rate()
--- models/keywords.py
+++ models/keywords.py
@@ -3,16 +3,25 @@ import json
 keywords_list = []
 
 def load_keywords():
-	iclr_data = open('./data/ICLR/keywords.json','r')
-	cvpr_data = open('./data/CVPR/keywords.json', 'r')
 	acl_data = open('./data/ACL/keywords.json','r')
+	cvpr_data = open('./data/CVPR/keywords.json', 'r')
+	emnlp_data = open('./data/EMNLP/keywords.json', 'r')
+	iclr_data = open('./data/ICLR/keywords.json','r')
+	icml_data = open('./data/ICML/keywords.json', 'r')
+	nips_data = open('./data/NIPS/keywords.json', 'r')
 
-	iclr = json.load(iclr_data)
-	cvpr = json.load(cvpr_data)
 	acl = json.load(acl_data)
+	cvpr = json.load(cvpr_data)
+	emnlp = json.load(emnlp_data)
+	iclr = json.load(iclr_data)
+	icml = json.load(icml_data)
+	nips = json.load(nips_data)
 
-	keywords_list.append(iclr)
-	keywords_list.append(cvpr)
 	keywords_list.append(acl)
-
+	keywords_list.append(cvpr)
+	keywords_list.append(emnlp)
+	keywords_list.append(iclr)
+	keywords_list.append(icml)
+	keywords_list.append(nips)
+	
 load_keywords()
