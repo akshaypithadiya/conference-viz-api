81bc62d38c7b03705442e9975caf883a6cd03aaa
Akshay Pithadiya <akshay.pithadiya@gmail.com>
2020-12-12 11:16:23 +0530

    added data for conference papers

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_cors import CORS
 from resources.conference import Conference, Conferences
 from resources.acceptance_rate import AcceptanceRate, AcceptanceRates
 from resources.keywords import AllKeywords, Keywords
+from resources.papers import AllPapers, Papers
 
 app = Flask(__name__)
 api = Api(app)
@@ -18,9 +19,13 @@ api.add_resource(Conference, '/conference/<string:_id>')
 api.add_resource(AcceptanceRates, '/conferences/acceptance-rates')
 api.add_resource(AcceptanceRate, '/conference/<string:_id>/acceptance-rate')
 
-#keywords of conference
+# keywords of conference
 api.add_resource(AllKeywords, '/conferences/keywords')
 api.add_resource(Keywords, '/conference/<string:_id>/keywords')
+
+# papers of conference
+api.add_resource(AllPapers, '/conferences/papers')
+api.add_resource(Papers, '/conference/<string:_id>/papers')
 
 if __name__ == "__main__":
 	app.run(port=5000, debug=True)
