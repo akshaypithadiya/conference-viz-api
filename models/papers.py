2cfdbe978201ca1c818caace30bcfd6f601afbc3
Akshay Pithadiya <akshay.pithadiya@gmail.com>
2020-12-14 19:26:53 +0530

    updated api with conferences

--- models/papers.py
+++ models/papers.py
@@ -2,26 +2,20 @@ import json
 
 papers_list = []
 
-def load_keywords():
+def load_papers():
+	aaai_data = open('./data/AAAI/papers.json', 'r')
 	acl_data = open('./data/ACL/papers.json','r')
 	cvpr_data = open('./data/CVPR/papers.json', 'r')
-	emnlp_data = open('./data/EMNLP/papers.json', 'r')
 	iclr_data = open('./data/ICLR/papers.json','r')
-	icml_data = open('./data/ICML/papers.json', 'r')
-	nips_data = open('./data/NIPS/papers.json', 'r')
 
+	aaai = json.load(aaai_data)
 	acl = json.load(acl_data)
 	cvpr = json.load(cvpr_data)
-	emnlp = json.load(emnlp_data)
 	iclr = json.load(iclr_data)
-	icml = json.load(icml_data)
-	nips = json.load(nips_data)
 
+	papers_list.append(aaai)
 	papers_list.append(acl)
 	papers_list.append(cvpr)
-	papers_list.append(emnlp)
 	papers_list.append(iclr)
-	papers_list.append(icml)
-	papers_list.append(nips)
 	
-load_keywords()
+load_papers()
